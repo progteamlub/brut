ec7744f0262afb986fd8d9ab6da335ef46456d17
progteamlub <69053955+progteamlub@users.noreply.github.com>
2020-08-16 18:27:34 +0500

    Add files via upload

---
+++ vtins.sh
@@ -0,0 +1,10 @@
+apt install python
+apt install toilet
+pip install --upgrade pip
+pip install time
+pip install os
+pip install vk
+pip install vk_api
+pip install colorama
+pip install random
+python vt.py
